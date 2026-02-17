024279244264a987c14bd9c5d1e0bf146632d8b1
vhr1757 <vedanshrajput1705@gmail.com>
2026-02-17 22:56:35 +0530

    Customized the admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,31 @@
+from datetime import datetime, timezone
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
 # Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Question", {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
